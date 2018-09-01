93c3d3d8f465d3b34090b1eb40a6b2e7c87fff8a
Thomas Fortune <tfortune88@gmail.com>
2018-09-01 16:36:59 -0700

    Cleaned out bugs and todo list
    Fixed duplicate itemIds in response,
    cleaned up the key that is stored in the map to only allow alphanumeric,
    store the key in lowercase, convert keyword to lowercase prior to
    fetching from map.

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ const Boom = require('boom')
 
 var productMap = [];
 
-// const prodIds = [14225185,
-// 14225186,
-// 14225188,
-// 14225187,
-// 39082884,
-// 30146244,
-// 12662817,
-// 34890820,
-// 19716431,
-// 42391766,
-// 35813552,
-// 40611708,
-// 40611825,
-// 36248492,
-// 44109840,
-// 23117408,
-// 35613901,
-// 42248076];
+// TODO: Read From CSV File
 
 const prodIds = [
     14225185,
     14225186,
+    14225188,
+    14225187,
+    39082884,
+    30146244,
+    12662817,
+    34890820,
+    19716431,
+    42391766,
+    35813552,
+    40611708,
+    40611825,
+    36248492,
+    44109840,
+    23117408,
+    35613901,
+    42248076
 ];
+
 
 // Create a server with a host and port
 const server=Hapi.server({
@@ -51,7 +51,7 @@ server.route({
     method:'GET',
     path:'/product/{keyword}',
     handler:function(request,h) {
-      var itemIds = productMap[encodeURIComponent(request.params.keyword)];
+      var itemIds = productMap[encodeURIComponent(request.params.keyword).toLowerCase()];
       if (itemIds == null) {
           return Boom.notFound("no products for that keyword are available");
       }
@@ -62,27 +62,35 @@ server.route({
 
 // Store each word in the string in a map where the key is the word and the value are the product ids
 function hashResults(aDescription, itemId) {
-//   console.log('shortDesc: ' + aDescription);
-
   var strSplit = aDescription.split(" ");
   var itemIds;
 
   for (var i = 0; i < strSplit.length; i++) {
-    // TODO: clean up word, remove commas, periods, apostrophes and quotes
+    var aWord = cleanTheWord(strSplit[i]);
 
-    // Only store strings larger the 2 chars
-    if (strSplit[i].length < 3) continue;
+    // Only store strings larger the 3 chars
+    if (aWord.length < 4) {
+        continue;
+    }
 
-    itemIds = productMap[strSplit[i]];
+    itemIds = productMap[aWord];
     if (itemIds == null) {
-        productMap[strSplit[i]] = itemId;
+        productMap[aWord] = itemId;
     } else {
-        // TODO: check to see if the id is already in the list before adding
-
-        productMap[strSplit[i]] = itemIds + ',' + itemId;
+        // Check to see if the id is already in the list before adding
+        if (itemIds.toString().indexOf(itemId.toString()) > -1) {
+            continue;
+        }
+        productMap[aWord] = itemIds + ',' + itemId;
     }
   }
+};
 
+// Strip out all non alpha numeric and force to lowercase
+function cleanTheWord(dirtyWord) {
+  var cleanedWord = dirtyWord.toLowerCase();
+  cleanedWord = cleanedWord.replace(/\W/g, '');
+  return cleanedWord;
 };
 
 // Load the data from the Walmart API into a Map before the server is ready to accept connections
@@ -101,9 +109,10 @@ function init() {
     sleep.sleep(1);
   }
 
-  for (var x in productMap) {
-    console.log("key: " + x + " -- value: " + productMap[x]);
-  }
+  // Dump the Map
+//   for (var x in productMap) {
+//     console.log("key: " + x + " -- value: " + productMap[x]);
+//   }
 
   console.log("init end");
 };
